c39755ee2aff4b8de647c4f3be159854d1d03535
Reminisce <euniceho1111@gmail.com>
2026-01-20 08:57:06 +0800

    Update DAY04_TASK2.py

--- DAY04_TASK2.py
+++ DAY04_TASK2.py
@@ -2,11 +2,19 @@
 try: 
 	a = int(input("Input an integer"))
 	b = int(input("Input another integer"))
-  
-	result = a/b
+
+	sum = a+b
+	difference = a-b
+	product = a*b
+	quotient = a/b
 except ZeroDivisionError:
 	print("cannot divide by zero")
 except ValueError:
 	print("not a number")
+finally:
+	print(sum)
+	print(difference)
+	print(product)
+	print(quotient)
   
   
